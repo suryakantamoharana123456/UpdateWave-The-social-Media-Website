af8f85bfdb2874f1f57a5f9767bd221c0cbb7ace
suryakantamoharana123456 <suryakantamoharana60019@gmail.com>
2025-06-17 09:33:38 +0530

    Delete src directory

--- src/Routes/Routers.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import Sidebar from "../components/Sidebar/Sidebar";
-const Routers = () => {
-  return (
-   <></>
-  );
-};
-
-export default Routers;
--- src/main/java/com/surya/exception/ChatException.java
+++
@@ -1,9 +0,0 @@
-package com.surya.exception;
-
-public class ChatException extends Exception {
-
-	public ChatException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
